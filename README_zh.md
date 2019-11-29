b0c8a40fad74741fb767710c9bca080ce140a619
Li Yang <liyang_tju@foxmail.com>
2019-11-29 20:03:36 +0800

    update: support more int type

--- README_zh.md
+++ README_zh.md
@@ -94,8 +94,14 @@ type Booleans struct {
 - string
 - bool
 - int
+- int8
+- int16
+- int32
 - int64
 - uint
+- uint8
+- uint16
+- uint32
 - uint64
 - float32
 - float64
--- test/config_test.go
+++ test/config_test.go
@@ -1,65 +1,71 @@
 package test
 
 import (
-	"encoding/json"
-	"fmt"
-	"os"
-	"testing"
+    "encoding/json"
+    "fmt"
+    "os"
+    "testing"
 
-	"github.com/tpgzcyyao/config"
+    "config"
 )
 
 type ResConfig struct {
-	StringSection  Strings
-	IntSection     Ints
-	FloatSection   Floats
-	BooleanSection Booleans
+    StringSection  Strings
+    IntSection     Ints
+    FloatSection   Floats
+    BooleanSection Booleans
 }
 
 type Strings struct {
-	StringFirst  string
-	StringSecond string
+    StringFirst  string
+    StringSecond string
 }
 
 type Ints struct {
-	IntFirst  int
-	IntSecond int64
-	IntThird  uint
-	IntForth  uint64
+    IntVal    int
+    Int8Val   int8
+    Int16Val  int16
+    Int32Val  int32
+    Int64Val  int64
+    UintVal   uint
+    Uint8Val  uint8
+    Uint16Val uint16
+    Uint32Val uint32
+    Uint64Val uint64
 }
 
 type Floats struct {
-	FloatFirst  float32
-	FloatSecond float64
+    FloatFirst  float32
+    FloatSecond float64
 }
 
 type Booleans struct {
-	BooleanFirst  bool
-	BooleanSecond bool
+    BooleanFirst  bool
+    BooleanSecond bool
 }
 
 func TestLoadFile(t *testing.T) {
-	file := "test.conf"
-	str, _ := os.Getwd()
-	resMap, err := (new(config.Config)).LoadFile(str + "/" + file)
-	if err != nil {
-		t.Errorf("LoadFile error: %v", err)
-	}
-	fmt.Println(fmt.Sprintf("The config map is: \n%v\n", resMap))
-	strJson, err := json.MarshalIndent(resMap, "", "\t")
-	if err != nil {
-		t.Errorf("json.Marshl error: %v", err)
-	}
-	fmt.Println(fmt.Sprintf("The config map in json format is: \n%s\n", strJson))
+    file := "test.conf"
+    str, _ := os.Getwd()
+    resMap, err := (new(config.Config)).LoadFile(str + "/" + file)
+    if err != nil {
+        t.Errorf("LoadFile error: %v", err)
+    }
+    fmt.Println(fmt.Sprintf("The config map is: \n%v\n", resMap))
+    strJson, err := json.MarshalIndent(resMap, "", "\t")
+    if err != nil {
+        t.Errorf("json.Marshl error: %v", err)
+    }
+    fmt.Println(fmt.Sprintf("The config map in json format is: \n%s\n", strJson))
 }
 
 func TestLoadConfig(t *testing.T) {
-	file := "test.conf"
-	str, _ := os.Getwd()
-	resConfig := new(ResConfig)
-	err := (new(config.Config)).LoadConfig(str+"/"+file, resConfig)
-	if err != nil {
-		t.Errorf("LoadConfig error: %v", err)
-	}
-	fmt.Println(fmt.Sprintf("The config struct is: \n%v\n", resConfig))
+    file := "test.conf"
+    str, _ := os.Getwd()
+    resConfig := new(ResConfig)
+    err := (new(config.Config)).LoadConfig(str+"/"+file, resConfig)
+    if err != nil {
+        t.Errorf("LoadConfig error: %v", err)
+    }
+    fmt.Println(fmt.Sprintf("The config struct is: \n%v\n", resConfig))
 }
